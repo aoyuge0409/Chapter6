a2d1a226fbe9fc0d5e2fda39a01e3e033ced8bb2
Andressa Yuge <yuriyuge04@gmail.com>
2020-02-25 18:51:08 -0500

    Chapter6

---
+++ Chapter6/Chapter6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Chapter6
+{
+    class Program
+    {
+        static void Write()
+        {
+            Console.WriteLine("Text OUtput from function.");
+        }
+        static void Main(string[] args)
+        {
+            Write();
+            Console.ReadKey();
+
+        }
+    }
+}
